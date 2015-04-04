fefc875e447d2a570bc2a1f52aca5c534f14b5f0
malakumar <kumam802@newschool.edu>
2015-04-03 21:17:03 -0400

    portfolio files for april 3, 2015

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
+<head>
+
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+
+<title>the world of disco cammata</title>
+
+<style type='text/css'> 
+#num {padding-left: 6px;}
+.img bot {margin-bottom: 6px; display: block;}
+</style> 
+
+<link rel='stylesheet' href='http://discocammata.com/ndxz-studio/site/theme_01/style.css' type='text/css' />
+<!--[if lte IE 6]>
+<link rel='stylesheet' href='http://discocammata.com/ndxz-studio/site/theme_01/ie.css' type='text/css' />
+<![endif]-->
+
+<script type='text/javascript' src='http://discocammata.com/ndxz-studio/site/js/jquery.js'></script> 
+<script type='text/javascript' src='http://discocammata.com/ndxz-studio/site/js/cookie.js'></script> 
+<script type='text/javascript' src='http://www.ednacional.com/indexhibit/ndxz-studio/site/js/jquery.cycle.all.js'></script>
+
+<script type='text/javascript'>
+path = 'http://discocammata.com/files/gimgs/';
+
+$(document).ready(function()
+{
+	setTimeout('move_up()', 1);
+});
+</script>
+<script type='text/javascript'>
+$(document).ready(function(){
+$('#s1').cycle({
+fx:'fade',
+speed:'2000',
+timeout: 0,
+next:'#next', prev:'#prev'});
+});
+</script> 
+
+</head>
+
+<body class='section-1'>
+	<div id='menu'>
+		<div class='container'>
+			<p>
+				<a href='http://discocammata.com/main/index' class='mainimage' ><img src='http://discocammata.com/spacer-icon6.gif'></a>
+			</p> 
+				<ul> 
+					<li class='section-title'>work</li>
+						
+						<li><a href='http://discocammata.com/work/interactive-guide/' class='title' onclick="do_click();">learning a new language</a></li>
+						<li><a href='http://discocammata.com/work/artificial-intimacy/' class='title' onclick="do_click();">artificial intimacy</a></li>
+						<li><a href='http://discocammata.com/work/creative-coding/' class='title' onclick="do_click();">creative coding</a></li>
+						<li><a href='http://discocammata.com/work/lost-in-google-translation/' class='title' onclick="do_click();">lost in google translation</a></li>
+						<li><a href='http://discocammata.com/work/5x5/' class='title' onclick="do_click();">5x5</a></li>
+						<li><a href='http://discocammata.com/work/experimental-narratives/' class='title' onclick="do_click();">experimental narratives</a></li>
+	
+						<li><a href='http://discocammata.com/work/disco-dancer/' class='title' onclick="do_click();">disco dancer</a></li>
+						<!-- <li><a href='http://discocammata.com/work/bootcamp-poster/' class='title' onclick="do_click();">bootcamp poster</a></li> -->
+						<li><a href='http://discocammata.com/work/wayfinding/' class='title' onclick="do_click();">wayfinding</a></li>
+
+						<li><a href='http://discocammata.com/work/lecture-posters/' class='title' onclick="do_click();">lecture posters</a></li> 
+						<li><a href='http://discocammata.com/work/how-to-disappear/' class='title' onclick="do_click();">how to disappear</a></li> 
+						<li><a href='http://discocammata.com/work/new-pinch-project/' class='title' onclick="do_click();">new pinch project</a></li> 
+							<!-- <li><a href='http://discocammata.com/work/rob-roy-kelly-wood-type-collection/' onclick="do_click();">rob roy kelly wood type collection</a></li> --> 
+							<!-- <li><a href='http://discocammata.com/work/breadcrumbs-to-pebbles/' onclick="do_click();">breadcrumbs to pebbles</a></li> --> 
+						<!-- <li><a href='http://discocammata.com/work/marcovaldo/' class='title' onclick="do_click();">marcovaldo</a></li>  -->
+							<!-- <li><a href='http://discocammata.com/work/this-is-your-destination/' onclick="do_click();">this is your destination</a></li> --> 
+						<li><a href='http://discocammata.com/work/my-grandfathers-stamps/' class='title' onclick="do_click();">stamp collector</a></li> 
+							<!-- <li><a href='http://discocammata.com/work/free-people-read-freely/' onclick="do_click();">free people read freely</a></li> --> 
+						<li><a href='http://discocammata.com/work/rishikesh/' class='title' onclick="do_click();">rishikesh</a></li> 
+<!-- 						<li><a href='http://discocammata.com/work/freelance/' class='title' onclick="do_click();">free agent</a></li>
+ -->						<li><a href='http://discocammata.com/work/older-work/' class='title' onclick="do_click();">more projects</a></li>
+				</ul> 
+
+					
+				<ul> 
+					<li class='section-title'>about</li> 
+					<li><a href='http://discocammata.com/info/the-girl' class='title' onclick="do_click();">the girl</a></li> 
+				</ul>
+ 
+				<!-- <ul> 
+					<li class='section-title'>links</li> 
+					<li><a href='http://discocammata.com/links/other-places/' onclick="do_click();">people and places</a></li> 
+				</ul>  -->
+
+				<ul> 
+					<li class='section-title'><a href='http://discocammata.com/main/index' class='indextitle' onclick="do_click();">index</a></li> 
+					<!-- <li><a href='http://discocammata.com/main/index' onclick="do_click();">index</a></li> --> 
+				</ul> 
+<!-- you must provide a link to Indexhibit on your site someplace - thank you -->
+			<!-- 	<ul>
+					<li>Built with <a href='http://www.indexhibit.org/'>Indexhibit</a></li>
+				</ul> -->
+
+	</div>	
+		</div>	
+
+<div id='content'>
+<div class='container'>
+
+<!-- text and image -->
+
+
+<plug:front_exhibit />
+ 
+
+<!-- end text and image -->
+
+</div>
+</div>
+
+</body>
+</html>
---
+++ original files/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
+<head>
+
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+
+<title>the world of disco cammata</title>
+
+<style type='text/css'> 
+#num {padding-left: 6px;}
+.img bot {margin-bottom: 6px; display: block;}
+</style> 
+
+<link rel='stylesheet' href='http://discocammata.com/ndxz-studio/site/theme_01/style.css' type='text/css' />
+<!--[if lte IE 6]>
+<link rel='stylesheet' href='http://discocammata.com/ndxz-studio/site/theme_01/ie.css' type='text/css' />
+<![endif]-->
+
+<script type='text/javascript' src='http://discocammata.com/ndxz-studio/site/js/jquery.js'></script> 
+<script type='text/javascript' src='http://discocammata.com/ndxz-studio/site/js/cookie.js'></script> 
+<script type='text/javascript' src='http://www.ednacional.com/indexhibit/ndxz-studio/site/js/jquery.cycle.all.js'></script>
+
+<script type='text/javascript'>
+path = 'http://discocammata.com/files/gimgs/';
+
+$(document).ready(function()
+{
+	setTimeout('move_up()', 1);
+});
+</script>
+<script type='text/javascript'>
+$(document).ready(function(){
+$('#s1').cycle({
+fx:'fade',
+speed:'2000',
+timeout: 0,
+next:'#next', prev:'#prev'});
+});
+</script> 
+
+</head>
+
+<body class='section-1'>
+<div id='menu'>
+<div class='container'>
+
+<p><a href="http://www.discocammata.com"><img src="http://discocammata.com/spacer-icon5.gif"></a></p> 
+
+<ul> 
+<li class='section-title'>work</li> 
+<li><a href='http://discocammata.com/work/lecture-posters/' onclick="do_click();">lecture posters</a></li> 
+<li><a href='http://discocammata.com/work/how-to-disappear/' onclick="do_click();">how to disappear</a></li> 
+<li><a href='http://discocammata.com/work/new-pinch-project/' onclick="do_click();">new pinch project</a></li> 
+<li><a href='http://discocammata.com/work/rob-roy-kelly-wood-type-collection/' onclick="do_click();">rob roy kelly wood type collection</a></li> 
+<li><a href='http://discocammata.com/work/breadcrumbs-to-pebbles/' onclick="do_click();">breadcrumbs to pebbles</a></li> 
+<li><a href='http://discocammata.com/work/marcovaldo/' onclick="do_click();">marcovaldo</a></li> 
+<li><a href='http://discocammata.com/work/this-is-your-destination/' onclick="do_click();">this is your destination</a></li> 
+<li><a href='http://discocammata.com/work/my-grandfathers-stamps/' onclick="do_click();">stamp collector</a></li> 
+<li><a href='http://discocammata.com/work/free-people-read-freely/' onclick="do_click();">free people read freely</a></li> 
+<li><a href='http://discocammata.com/work/rishikesh/' onclick="do_click();">rishikesh</a></li> 
+<li><a href='http://discocammata.com/work/freelance/' onclick="do_click();">free agent</a></li> 
+
+
+
+</ul> 
+<ul> 
+<li class='section-title'>about</li> 
+<li><a href='http://discocammata.com/info/the-girl' onclick="do_click();">the girl</a></li> 
+</ul>
+ 
+<ul> 
+<li class='section-title'>links</li> 
+<li><a href='http://discocammata.com/links/other-places/' onclick="do_click();">people and places</a></li> 
+</ul> 
+
+<ul> 
+<li class='section-title'>main</li> 
+<li><a href='http://discocammata.com/main/index' onclick="do_click();">index</a></li> 
+
+</ul> 
+
+
+
+</ul> 
+<!-- you must provide a link to Indexhibit on your site someplace - thank you -->
+<ul>
+<li>Built with <a href='http://www.indexhibit.org/'>Indexhibit</a></li>
+</ul>
+
+</div>	
+</div>	
+
+<div id='content'>
+<div class='container'>
+
+<!-- text and image -->
+
+
+<plug:front_exhibit />
+ 
+
+<!-- end text and image -->
+
+</div>
+</div>
+
+</body>
+</html>
